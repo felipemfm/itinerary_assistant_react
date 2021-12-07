444be4fbf7066130e9c125e27141308a06a9babe
felipemfm <felipemfm@gmail.com>
2021-12-07 13:41:21 +0900

    first commit

---
+++ README copy.md
@@ -0,0 +1,6 @@
+# On developement Project
+
+This project is based on a previous project developed by me (https://github.com/felipemfm/alarm_project)
+Recreated using the React javascript Framework.
+
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const App = () => {
+  const operatorList = ["JR-East", "TokyoMetro", "Toei"];
+  const [operator, setOperator] = useState("");
+  const [line, setLine] = useState("");
+  const [lineList, setLineList] = useState([]);
+
+  useEffect(() => {
+    const getLine = async () => {
+      const { data } = await axios.get(
+        `https://api-tokyochallenge.odpt.org/api/v4/odpt:Railway?odpt:operator=odpt.Operator:${operator}&acl:consumerKey=${apiKey}`
+      );
+      // console.log(data);
+      setLineList(data);
+    };
+    getLine();
+  }, [operator]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="form-group">
+        <label className="form-label">Line Operator</label>
+        <select
+          value={operator}
+          className="form-control form-select"
+          onChange={(e) => setOperator(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option key="0" defaultValue=""></option>
+          {operatorList.map((element, index) => {
+            return (
+              <option key={index + 1} value={element}>
+                {element}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="form-group">
+        <label className="form-label">Train Line</label>
+        <select
+          value={line}
+          className="form-control form-select"
+          onChange={(e) => setLine(e.target.value)}
+        >
+          <option key="0" defaultValue=""></option>
+          {lineList.map((data, index)=>{
+              return data["odpt:stationOrder"].length > 1 ? (
+                <option key={index+1} defaultValue={data["owl:sameAs"]}>
+                  {data["odpt:railwayTitle"]["en"]}
+                </option>
+              ) : " ";
+          })}
+        </select>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
